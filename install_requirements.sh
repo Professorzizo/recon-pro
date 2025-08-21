d25006e8ce32d0911ac00216a7143488d3026bd1
sadiQ-HASHIM <105419423+Professorzizo@users.noreply.github.com>
2025-08-21 15:16:58 +0200

    Update and rename requirements.txt to install_requirements.sh

---
+++ install_requirements.sh
@@ -0,0 +1,14 @@
+# Pro Recon
+
+Professional Recon pipeline in Python with CLI options.
+
+## Requirements
+
+- Linux / WSL
+- Python3 + pip
+- Go installed
+
+### Install all dependencies
+
+```bash
+bash install_requirements.sh
